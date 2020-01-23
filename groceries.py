87cd938bf166b1d89dd6418286b9ba9b4987c7df
Mike Rossetti <s2t2@users.noreply.github.com>
2020-01-23 01:31:46 -0500

    end of class results

--- groceries.py
+++ groceries.py
@@ -10,7 +10,7 @@ products = [
     {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
     {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
     {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    #{"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
     {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
     {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
     {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
@@ -25,7 +25,40 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
-# pprint(products)
+print("--------------")
+#print("THERE ARE 20 PRODUCTS:")
+#print("THERE ARE ", len(products), " PRODUCTS:")
+#print("THERE ARE " + str(len(products)) + " PRODUCTS:")
+print(f"THERE ARE {len(products)} PRODUCTS:")
+print("--------------")
 
-# TODO: write some Python code here to produce the desired output
+
+for p in products:
+    print(p["name"])
+
+
+
+# --------------
+# THERE ARE 20 PRODUCTS:
+# --------------
+#  + All-Seasons Salt ($4.99)
+#  + Chocolate Fudge Layer Cake ($18.50)
+#  + Chocolate Sandwich Cookies ($3.50)
+#  + Cut Russet Potatoes Steam N' Mash ($4.25)
+#  + Dry Nose Oil ($21.99)
+#  + Fresh Scent Dishwasher Cleaner ($4.99)
+#  + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+#  + Green Chile Anytime Sauce ($7.99)
+#  + Light Strawberry Blueberry Yogurt ($6.50)
+#  + Mint Chocolate Flavored Syrup ($4.50)
+#  + Overnight Diapers Size 6 ($25.50)
+#  + Peach Mango Juice ($1.99)
+#  + Pizza For One Suprema Frozen Pizza ($12.50)
+#  + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+#  + Pure Coconut Water With Orange ($3.50)
+#  + Rendered Duck Fat ($9.99)
+#  + Robust Golden Unsweetened Oolong Tea ($2.49)
+#  + Saline Nasal Mist ($16.00)
+#  + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+#  + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+#
